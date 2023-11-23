813cc683b39c81554350a3ce0c054cc5457f0756
David <d.woodward@gmx.de>
2023-11-23 20:10:38 +0100

    Add bcm lib; add blinky code; add gitattribs

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
+#include "bcm2835.h"
 
+
+static constexpr int outPin = RPI_GPIO_P1_18;   // GPIO 24
 
 int main(void) {
     std::cout << "Hello rpi-logger!" << std::endl;
+
+    if (!bcm2835_init()) {
+        std::cout << "FAILED TO INIT BCM2835!!" << std::endl;
+        return 1;
+    }
+
+    bcm2835_gpio_fsel(outPin, BCM2835_GPIO_FSEL_OUTP);
+
+    for (int i = 0; i < 100; i++) {
+        bcm2835_gpio_write(outPin, HIGH);
+        std::cout << ":" << std::flush;
+        bcm2835_delay(500);
+        bcm2835_gpio_write(outPin, LOW);
+        std::cout << "." << std::flush;
+        bcm2835_delay(500);
+    }
+    std::cout << std::endl;
+
+    bcm2835_close();
     return 0;
 }
