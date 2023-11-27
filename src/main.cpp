658ebf7d2e8a99e301ce855612036629813dbcc1
David <d.woodward@gmx.de>
2023-11-27 23:11:18 +0100

    test object-oriented DigitalIo interface

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,15 @@
 #include <iostream>
 #include <chrono>
 #include <thread>
-// #include "hal/gpio.h"
-// #include "hal/gpioPin.h"
 // #include "driver/Dht11.h"
 #include "hal/IGpio.h"
 #include "hal/bcm2835/Gpio.h"
+#include "driver/IDigitalIo.h"
+#include "driver/DigitalIo.h"
 
 
 int main(void) {
     std::cout << "Hello rpi-logger!" << std::endl;
-
-    // if (!gpio::init()) {
-    //     std::cout << "Failed to init GPIO!" << std::endl;
-    //     return 1;
-    // }
 
     hal::bcm2835::Gpio gpio;
     if (!gpio.init()) {
@@ -22,21 +17,21 @@ int main(void) {
         return 1;
     }
 
-    gpio.initOutputPin(24);
-    for (int i = 0; i < 10; i++) {
-        gpio.setPinHigh(24);
-        std::this_thread::sleep_for(std::chrono::milliseconds(500));
-        gpio.setPinLow(24);
-        std::this_thread::sleep_for(std::chrono::milliseconds(500));
-    }
-
-    // gpio::OutputPin pin(24);
+    // gpio.initOutputPin(24);
     // for (int i = 0; i < 10; i++) {
-    //     pin.setHigh();
+    //     gpio.setPinHigh(24);
     //     std::this_thread::sleep_for(std::chrono::milliseconds(500));
-    //     pin.setLow();
+    //     gpio.setPinLow(24);
     //     std::this_thread::sleep_for(std::chrono::milliseconds(500));
     // }
+
+    DigitalOutput pin(gpio, 24);
+    for (int i = 0; i < 10; i++) {
+        pin.setHigh();
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        pin.setLow();
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    }
 
     // gpio::cleanup();
     return 0;
