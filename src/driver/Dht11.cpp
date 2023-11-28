7e7e79f853ca384f301bbefc6a3626cf42dc0170
David <d.woodward@gmx.de>
2023-11-28 22:42:09 +0100

    Improve read algorithm; move time delta -> bit value decoding out of read loop

--- src/driver/Dht11.cpp
+++ src/driver/Dht11.cpp
@@ -13,10 +13,9 @@ void Dht11::poll(void) {
     getData();
 
     bool levelIsOk = false;
-    for (int t = 0; t < 20000; t++) {
+    for (int t = 0; t < 200; t++) {
+        std::this_thread::sleep_for(std::chrono::microseconds(1));
         if (pin->getLevel() == hal::PinLevel::Low) {
-            // TODO: If I remove this line, sensor reading fails at step 3
-            std::cout << "Level is OK (1), t = " << t << "\n";
             levelIsOk = true;
             break;
         }
@@ -26,9 +25,10 @@ void Dht11::poll(void) {
         return;
     }
 
+
     std::chrono::steady_clock::time_point tStart;
     std::chrono::steady_clock::time_point tEnd;
-    int buffer[41];
+    unsigned long long deltaBuffer[41];
 
     for (int i = 0; i < 41; i++) {
         levelIsOk = false;
@@ -45,40 +45,47 @@ void Dht11::poll(void) {
         }
 
         levelIsOk = false;
-        for (int t = 0; t < 20000; t++) {
+        int t = 0;
+        for (t = 0; t < 200000; t++) {
+            // std::this_thread::sleep_for(std::chrono::microseconds(1));
             if (pin->getLevel() == hal::PinLevel::Low) {
                 tEnd = std::chrono::steady_clock::now();
                 levelIsOk = true;
+                // std::cout << "(3) t = " << t << "\n";
                 break;
             }
         }
         if (!levelIsOk) {
-            std::cout << "Level is not OK (3)\n";
+            std::cout << "Level is not OK (3), t = " << t << "  i = " << i << "\n";
             return;
         }
 
         auto tDelta = std::chrono::duration_cast<std::chrono::microseconds>(tEnd - tStart);
-        long long deltaUs = tDelta.count();
+        deltaBuffer[i] = tDelta.count();
+    }
 
-        long long delta70 = std::labs(70 - deltaUs);
-        long long delta26 = std::labs(26 - deltaUs);
-        std::cout << "delta70: " << delta70 << "  delta26: " << delta26 << std::endl;
+
+    // Translate deltas to bits
+    int bitBuffer[41];
+    for (int i = 0; i < 41; i++) {
+        long long delta70 = std::labs(70 - deltaBuffer[i]);
+        long long delta26 = std::labs(26 - deltaBuffer[i]);
+
+        std::cout << "[" << i << "] delta: " << deltaBuffer[i] << "  delta70: " << delta70 << "  delta26: " << delta26 << std::endl;
+
         if (delta70 < delta26) {
             // 1
-            buffer[i] = 1;
+            bitBuffer[i] = 1;
         }
         else {
             // 0
-            buffer[i] = 0;
+            bitBuffer[i] = 0;
         }
     }
 
-    // std::bitset<64> bufferBits(buffer);
-    // std::cout << "bufferBits: " << bufferBits << std::endl;
-    
-    std::cout << "buffer bits: ";
+    std::cout << "bitBuffer: ";
     for (int i = 0; i < 41; i++) {
-        std::cout << buffer[i];
+        std::cout << bitBuffer[i];
     }
     std::cout << std::endl;
 }
