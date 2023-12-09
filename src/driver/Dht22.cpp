ea46edf252c512217e2f0fbbafcf9ec7519f70c2
David <d.woodward@gmx.de>
2023-12-09 02:00:45 +0100

    use static_cast

--- src/driver/Dht22.cpp
+++ src/driver/Dht22.cpp
@@ -6,13 +6,13 @@ Dht22::Dht22(IDigitalReconfigurableIo& pin): DhtBase(pin) {
 }
 
 void Dht22::updateHumidity(const std::array<uint8_t, 5>& byteBuffer) {
-    float humidity = (float) ((byteBuffer[0] << 8) + (byteBuffer[1]));
+    float humidity = static_cast<float>((byteBuffer[0] << 8) + (byteBuffer[1]));
     humidity /= 10.0f;
     this->humidity = humidity;
 }
 
 void Dht22::updateTemperature(const std::array<uint8_t, 5>& byteBuffer) {
-    float temperature = (float) (((byteBuffer[2] && 0x7Fu) << 8) + byteBuffer[3]);
+    float temperature = static_cast<float>(((byteBuffer[2] && 0x7Fu) << 8) + byteBuffer[3]);
     temperature /= 10.0f;
     int negative = byteBuffer[2] & 0x80u;
     if (negative == 1) {
