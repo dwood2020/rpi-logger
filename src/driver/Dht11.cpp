625a7b70d6783873a9c74d704dbc3e71c4c816e8
David <d.woodward@gmx.de>
2023-12-09 02:15:26 +0100

    Implement Dht11 decoding

--- src/driver/Dht11.cpp
+++ src/driver/Dht11.cpp
@@ -6,11 +6,13 @@ Dht11::Dht11(IDigitalReconfigurableIo& pin): DhtBase(pin) {
 }
 
 void Dht11::updateHumidity(const std::array<uint8_t, 5>& byteBuffer) {
-    for (int i = 0; i < 5; i++) {
-        printf("0x%02X ");
-    }
-    printf("\n");
+    humidity = static_cast<float>(byteBuffer[0]);   // byte 1 is unused.
 }
 
 void Dht11::updateTemperature(const std::array<uint8_t, 5>& byteBuffer) {
+    // NOTE: Other sources say that for DHT11 devices, both bytes 1 and 3 are unused.
+    // This may be incorrect.
+    float whole = static_cast<float>(byteBuffer[2]);
+    float decimal = static_cast<float>(byteBuffer[3]);
+    temperature = whole + (decimal / 10.0f);
 }
