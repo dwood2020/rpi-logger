12b4af52e21d0876e2ff7dd22d1da3e03ed94b4c
David <d.woodward@gmx.de>
2023-12-11 21:51:03 +0100

    Add nlohmann/json + AppConfig

---
+++ src/application/AppConfig.cpp
@@ -0,0 +1,55 @@
+#include "AppConfig.h"
+#include <filesystem>
+#include <fstream>
+#include "nlohmann/json.hpp"
+
+
+void AppConfig::parse(std::string_view configFileDir) {
+    std::filesystem::path filepath = configFileDir;
+    if (!std::filesystem::is_directory(filepath)) {
+        filepath = filepath.parent_path();
+    }
+    filepath.append(appConfigFileName);
+
+    std::ifstream f(filepath);
+    if (!f.good()) {
+        throw std::runtime_error("Could not read config file");
+    }
+    nlohmann::json cfgData = nlohmann::json::parse(f);
+
+    if (cfgData.contains("logIntervalSec")) {
+        this->logIntervalSec = cfgData["logIntervalSec"];
+    }
+    if (cfgData.contains("dht11Pins")) {
+        auto dht11Pins = cfgData["dht11Pins"];
+        if (dht11Pins.is_array()) {
+            for (const auto& pin : dht11Pins) {
+                this->dht11Pins.push_back(pin);
+            }
+        }
+    }
+    if (cfgData.contains("dht22Pins")) {
+        auto dht22Pins = cfgData["dht22Pins"];
+        if (dht22Pins.is_array()) {
+            for (const auto& pin : dht22Pins) {
+                this->dht22Pins.push_back(pin);
+            }
+        }
+    }
+}
+
+const std::vector<hal::PinNumber_t>& AppConfig::getDht11Pins(void) const {
+    return dht11Pins;
+}
+
+const std::vector<hal::PinNumber_t>& AppConfig::getDht22Pins(void) const {
+    return dht22Pins;
+}
+
+int AppConfig::getLogIntervalSec(void) const {
+    return logIntervalSec;
+}
+
+std::string AppConfig::getCsvOutputDir(void) const {
+    return std::move(csvOutputDir);
+}
---
+++ src/application/AppConfig.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <string>
+#include <vector>
+#include "hal/IGpio.h"
+
+
+class AppConfig {
+private:
+    std::vector<hal::PinNumber_t> dht11Pins{};
+    std::vector<hal::PinNumber_t> dht22Pins{};
+    int logIntervalSec = 60;
+    std::string csvOutputDir;
+
+public:
+    static constexpr auto appConfigFileName = "rpiLoggerConfig.json";
+
+    AppConfig() = default;
+    virtual ~AppConfig() = default;
+
+    void parse(std::string_view configFileDir);
+
+    const std::vector<hal::PinNumber_t>& getDht11Pins(void) const;
+    const std::vector<hal::PinNumber_t>& getDht22Pins(void) const;
+    int getLogIntervalSec(void) const;
+    std::string getCsvOutputDir(void) const;
+};
