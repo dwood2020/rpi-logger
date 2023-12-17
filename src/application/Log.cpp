2b706a6f07888f03ba22a24dff095ae2aa061fe5
David <d.woodward@gmx.de>
2023-12-17 20:45:48 +0100

    disable generation of default elpp log file

--- src/application/Log.cpp
+++ src/application/Log.cpp
@@ -8,10 +8,10 @@ void Log::init(const std::filesystem::path& logfilePath) {
     el::Configurations cfg;
     cfg.setGlobally(el::ConfigurationType::Format, "%datetime{%H:%m:%s.%g} %level %msg");
     if (std::filesystem::exists(logfilePath)) {
-        cfg.setGlobally(el::ConfigurationType::ToFile, "true");
         auto path = logfilePath;
         path.append("rpiLoggerAppLog.txt");
         cfg.setGlobally(el::ConfigurationType::Filename, path.string().c_str());
+        cfg.setGlobally(el::ConfigurationType::ToFile, "true");
     }
 
     el::Loggers::setDefaultConfigurations(cfg, true);
