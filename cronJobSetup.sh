39ffd218d3cade4d836aaad290e5393221b78d69
David <d.woodward@gmx.de>
2024-01-03 21:00:24 +0100

    add cronjob setup script - untested

---
+++ cronJobSetup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Installation path of executable
+APP_PATH="/home/username/logger/rpi-logger"
+
+(crontab -l 2>/dev/null; echo "@reboot $APP_PATH") | crontab -
+echo "Cron job set up successfully. App will be launched automatically on every reboot."
