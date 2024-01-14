ea55508b2661abd64ca78528051eac86d9554b08
David <d.woodward@gmx.de>
2024-01-14 15:56:23 +0100

    Add scripts to install and remove systemd start service

---
+++ autostart/installStartService.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+# ==============================================================================
+# Creates a systemd service which launches the `rpi-logger` executable after
+# every system reboot.
+# 
+# Usage: Copy this script to *exactly* the installation directory of the
+# rpi-logger executable and run from there with
+# `sudo ./installStartService.sh` (chmod +x may be necessary).
+# ==============================================================================
+
+# Create service file.
+SERVICE_NAME="startRpiLogger"
+FILE_NAME=$SERVICE_NAME".service"
+DEST_PATH="/etc/systemd/system/${FILE_NAME}"
+
+FILE_CONTENT="[Unit]
+Description=Launches rpi-logger.
+
+[Service]
+WorkingDirectory="$PWD"
+ExecStart="$PWD"/rpi-logger
+
+[Install]
+WantedBy=multi-user.target"
+
+echo "$FILE_CONTENT" > $FILE_NAME
+
+
+# Install and setup service.
+mv "$FILE_NAME" "$DEST_PATH"
+chmod 644 "$DEST_PATH"
+
+systemctl start "$SERVICE_NAME"
+systemctl status "$SERVICE_NAME"
+
+STATUS_RESULT=$?
+echo "Status result:"
+echo "$STATUS_RESULT"
+
+systemctl enable "$SERVICE_NAME"
---
+++ autostart/removeStartService.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# ==============================================================================
+# Removes the installed start service from the system.
+# ==============================================================================
+
+SERVICE_NAME="startRpiLogger"
+FILE_NAME=$SERVICE_NAME".service"
+
+PATH1="/etc/systemd/system/${FILE_NAME}"
+PATH2="/usr/lib/systemd/system/${FILE_NAME}"
+
+systemctl stop "$SERVICE_NAME"
+systemctl disable "$SERVICE_NAME"
+
+rm "$PATH1"
+rm "$PATH2"
+
+systemctl daemon-reload
+systemctl reset-failed
