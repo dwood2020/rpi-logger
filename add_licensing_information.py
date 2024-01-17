83a704ade9098d8a18fa15a9c9b0a4f4583f392c
David <d.woodward@gmx.de>
2024-01-17 23:23:27 +0100

    ignore files that already have the licensing information

--- add_licensing_information.py
+++ add_licensing_information.py
@@ -1,15 +1,18 @@
+# Add licensing information to the top of every C++ source and header file.
+
 import os
 
-g_license_text: str = """
-/*
+g_license_text: str = """/*
  * Copyright (C) 2024  David Woodward
  * SPDX-License-Identifier: GPL-3.0-or-later
- */
-"""
+ */"""
 
 def add_license_comment(file_path: str) -> None:
     with open(file_path, 'r') as file:
         content = file.read()
+
+    if content.startswith(g_license_text):
+        return
 
     with open(file_path, 'w') as file:
         file.write(g_license_text + '\n\n' + content)
@@ -18,10 +21,12 @@ def add_license_comment(file_path: str) -> None:
 def process_files(dir_path: str) -> None:
     for root, _, files in os.walk(dir_path):
         for file in files:
-            if file.endswith(('.cpp', '.h')):
-                file_path: str = os.path.join(root, file)
-                add_license_comment(file_path)
-                print(f"Licensing text added to {file_path}")
+            if not file.endswith(('.cpp', '.h')):
+                continue
+
+            file_path: str = os.path.join(root, file)
+            add_license_comment(file_path)
+            print(f"Licensing information added to '{file_path}'")
 
 
 def main() -> None:
